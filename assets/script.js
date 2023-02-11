dbdbaae008928fca01922011fe7881eae97ff32d
trace-martin <trace.martin7817@gmail.com>
2023-02-10 21:33:15 -0700

    added links

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var topics = ['HTML', 'CSS', 'Git', 'JacaScript'];
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
